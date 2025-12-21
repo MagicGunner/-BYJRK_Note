785d1b6a2ba7ca529af9a70af1e4b3c92810e659
MissBlue <magicguner124@gmail.com>
2025-12-22 04:09:08 +0800

    WPFStyles

---
+++ WPF/WPFStyles/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFStyles;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    private void MainWindow_OnLoaded(object sender, RoutedEventArgs e) {
+        var style = new Style();
+        style.Setters.Add(new Setter(System.Windows.Controls.Panel.BackgroundProperty, Brushes.LightCoral));
+        style.Setters.Add(new Setter(TextElement.FontSizeProperty, 32d));
+        Panel.Style = style;
+    }
+}
